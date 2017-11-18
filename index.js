b63b9c01c003b06ccabefd939d561cf1b62533f2
James Gomis <jamesislike@gmail.com>
2017-11-18 15:03:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-class BoardMember{
-  construction(name, homeState, training)
-  this.name = name
-  this.homeState = homeState
-  this.training = training
-}
-
-veto(){return `No, I disagree`}
-approve(){return ``}
-doCharity(){return ``}
-releasePressStatement(){return ``}
-sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was tained in ${this.training}`}
-
-
-class Ceo extends BoardMember{
-  hireEmployee(){return 'Welcome aboard!'}
+class BoardMember {
+  constructor(name,homeState,training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+    veto() = {
+      return `No, I must disagree`
+    }
+    approve() = {
+      return `You can do that!`
+    }
+  }
 }
